d839a40025621cbe499aa30402b5bc5c08018029
pimdewaard <pim.dewaard@gmail.com>
2016-12-11 23:10:15 +0100

    single line graph d3

---
+++ d3line/d3line.js
@@ -0,0 +1,96 @@
+var margin = {top: 20, right: 50, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%m%d").parse,
+    formatTime = d3.time.format("%e %B"),
+    bisectDate = d3.bisector(function(d) { return d.Date; }).left,
+    formatValue = d3.format(",.1f"),
+    formatCurrency = function(d) { return formatValue(d) + "℃"; };
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.Date); })
+    .y(function(d) { return y(d.Average); });
+
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("temp1915.json", function(error, data) {
+			data.forEach(function(d) {
+				d.Date = parseDate(d.Date);
+				d.Average = +(d.Average/10);
+			});
+
+
+	x.domain(d3.extent(data, function(d) { return d.Date; }));
+	y.domain([-5, d3.max(data, function(d) { return d.Average; })]);
+
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Temperature (℃)");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .style("stroke", "blue")
+      .attr("d", line);
+
+  var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+  focus.append("circle")
+      .attr("r", 6);
+
+  focus.append("text")
+      .attr("x", 9)
+      .attr("dy", ".35em");
+
+  svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  function mousemove() {
+    var x0 = x.invert(d3.mouse(this)[0]),
+        i = bisectDate(data, x0, 1),
+        d0 = data[i - 1],
+        d1 = data[i],
+        d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+    focus.attr("transform", "translate(" + x(d.Date) + "," + y(d.Average) + ")");
+    focus.select("text").text(formatCurrency(d.Average));
+  }
+});
